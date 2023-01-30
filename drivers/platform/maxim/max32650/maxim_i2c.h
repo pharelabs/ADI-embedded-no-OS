pharelabs/ADI-embedded-no-OS

commit 25b4699e9cc0bd5d15e5edb3d9986769df9bde75

drivers:platform:maxim: Allow I2C vddio selection
Add the vssel field, so that the VDDIO may be configured for each I2C
interface. Adapt the projects accordingly.

Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/platform/maxim/max32650/maxim_i2c.h
@@ -59,6 +59,10 @@ struct max_i2c_extra {
 	uint8_t prologue_size;
 };
 
+struct max_i2c_init_param {
+	mxc_gpio_vssel_t vssel;
+};
+
 /**
  * @enum max_i2c_speed
  * @brief MAXIM I2C SCL frequency options

drivers/platform/maxim/max32655/maxim_i2c.h
@@ -43,6 +43,7 @@
 
 #include <stdint.h>
 #include "i2c_regs.h"
+#include "max32655.h"
 
 /**
  * @struct max_i2c_extra
@@ -57,6 +58,10 @@ struct max_i2c_extra {
 	/** Size of the write data in case of
 	 *  write-repeated start-read operation */
 	uint8_t prologue_size;
+};
+
+struct max_i2c_init_param {
+	mxc_gpio_vssel_t vssel;
 };
 
 /**

drivers/platform/maxim/max32660/maxim_i2c.h
@@ -43,6 +43,7 @@
 
 #include <stdint.h>
 #include "i2c_regs.h"
+#include "max32660.h"
 
 /**
  * @struct max_i2c_extra
@@ -57,6 +58,10 @@ struct max_i2c_extra {
 	/** Size of the write data in case of
 	 *  write-repeated start-read operation */
 	uint8_t prologue_size;
+};
+
+struct max_i2c_init_param {
+	mxc_gpio_vssel_t vssel;
 };
 
 /**

drivers/platform/maxim/max32665/maxim_i2c.h
@@ -43,6 +43,7 @@
 
 #include <stdint.h>
 #include "i2c_regs.h"
+#include "max32665.h"
 
 /**
  * @struct max_i2c_extra
@@ -57,6 +58,10 @@ struct max_i2c_extra {
 	/** Size of the write data in case of
 	 *  write-repeated start-read operation */
 	uint8_t prologue_size;
+};
+
+struct max_i2c_init_param {
+	mxc_gpio_vssel_t vssel;
 };
 
 /**

drivers/platform/maxim/max78000/maxim_i2c.h
@@ -43,6 +43,7 @@
 
 #include <stdint.h>
 #include "i2c_regs.h"
+#include "max78000.h"
 
 #ifndef MXC_I2C_GET_I2C
 #define MXC_I2C_GET_I2C(i)	((i) == 0 ? MXC_I2C0 :		\
@@ -63,6 +64,10 @@ struct max_i2c_extra {
 	/** Size of the write data in case of
 	 *  write-repeated start-read operation */
 	uint8_t prologue_size;
+};
+
+struct max_i2c_init_param {
+	mxc_gpio_vssel_t vssel;
 };
 
 /**

drivers/platform/maxim/max32650/maxim_i2c.c
@@ -80,6 +80,33 @@ void I2C2_IRQHandler(void)
 #endif
 
 /**
+ * @brief Configure the VDDIO level for a I2C interface
+ * @param device_id - the interface number.
+ * @param vssel - the VDDIO level.
+ * @return 0 in case of success, -EINVAL otherwise.
+ */
+static int32_t _max_i2c_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
+{
+	mxc_gpio_cfg_t *i2c_pins;
+
+	switch (device_id) {
+	case 0:
+		i2c_pins = &gpio_cfg_i2c0;
+		break;
+	case 1:
+		i2c_pins = &gpio_cfg_i2c1;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	i2c_pins->vssel = vssel;
+	MXC_GPIO_Config(i2c_pins);
+
+	return 0;
+}
+
+/**
  * @brief Initialize the I2C communication peripheral.
  * Supported bitrates are between 100kHz and 400 kHz.
  * Is slave address is 0, then this instance will be used for general call.
@@ -93,13 +120,14 @@ void I2C2_IRQHandler(void)
 static int32_t max_i2c_init(struct no_os_i2c_desc **desc,
 			    const struct no_os_i2c_init_param *param)
 {
+	struct max_i2c_init_param *eparam;
 	int32_t ret;
 	struct max_i2c_extra *max_i2c;
 	mxc_i2c_regs_t *i2c_regs;
 	uint32_t current_freq = 0;
 	uint32_t freq;
 
-	if (!desc || !param)
+	if (!desc || !param || !param->extra)
 		return -EINVAL;
 	if (param->device_id >= MXC_I2C_INSTANCES)
 		return -EINVAL;
@@ -114,6 +142,7 @@ static int32_t max_i2c_init(struct no_os_i2c_desc **desc,
 		goto error_desc;
 	}
 
+	eparam = param->extra;
 	i2c_regs = MXC_I2C_GET_I2C(param->device_id);
 	max_i2c->handler = i2c_regs;
 	(*desc)->device_id = param->device_id;
@@ -140,6 +169,11 @@ static int32_t max_i2c_init(struct no_os_i2c_desc **desc,
 			goto error_extra;
 		}
 	}
+
+	ret = _max_i2c_pins_config((*desc)->device_id, eparam->vssel);
+	if (ret)
+		return ret;
+
 	freq = MXC_I2C_GetFrequency(i2c_regs);
 	freq = no_os_min(freq, (*desc)->max_speed_hz);
 	ret = MXC_I2C_SetFrequency(i2c_regs, freq);

drivers/platform/maxim/max32655/maxim_i2c.c
@@ -80,6 +80,36 @@ void I2C2_IRQHandler(void)
 #endif
 
 /**
+ * @brief Configure the VDDIO level for a I2C interface
+ * @param device_id - the interface number.
+ * @param vssel - the VDDIO level.
+ * @return 0 in case of success, -EINVAL otherwise.
+ */
+static int32_t _max_i2c_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
+{
+	mxc_gpio_cfg_t *i2c_pins;
+
+	switch (device_id) {
+	case 0:
+		i2c_pins = &gpio_cfg_i2c0;
+		break;
+	case 1:
+		i2c_pins = &gpio_cfg_i2c1;
+		break;
+	case 2:
+		i2c_pins = &gpio_cfg_i2c2;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	i2c_pins->vssel = vssel;
+	MXC_GPIO_Config(i2c_pins);
+
+	return 0;
+}
+
+/**
  * @brief Initialize the I2C communication peripheral.
  * Supported bitrates are between 100kHz and 400 kHz.
  * Is slave address is 0, then this instance will be used for general call.
@@ -93,13 +123,14 @@ void I2C2_IRQHandler(void)
 static int32_t max_i2c_init(struct no_os_i2c_desc **desc,
 			    const struct no_os_i2c_init_param *param)
 {
+	struct max_i2c_init_param *eparam;
 	int32_t ret;
 	struct max_i2c_extra *max_i2c;
 	mxc_i2c_regs_t *i2c_regs;
 	uint32_t current_freq = 0;
 	uint32_t freq;
 
-	if (!desc || !param)
+	if (!desc || !param || !param->extra)
 		return -EINVAL;
 	if (param->device_id >= MXC_I2C_INSTANCES)
 		return -EINVAL;
@@ -114,6 +145,7 @@ static int32_t max_i2c_init(struct no_os_i2c_desc **desc,
 		goto error_desc;
 	}
 
+	eparam = param->extra;
 	i2c_regs = MXC_I2C_GET_I2C(param->device_id);
 	max_i2c->handler = i2c_regs;
 	(*desc)->device_id = param->device_id;
@@ -140,6 +172,11 @@ static int32_t max_i2c_init(struct no_os_i2c_desc **desc,
 			goto error_extra;
 		}
 	}
+
+	ret = _max_i2c_pins_config((*desc)->device_id, eparam->vssel);
+	if (ret)
+		return ret;
+
 	freq = MXC_I2C_GetFrequency(i2c_regs);
 	freq = no_os_min(freq, (*desc)->max_speed_hz);
 	ret = MXC_I2C_SetFrequency(i2c_regs, freq);

drivers/platform/maxim/max32660/maxim_i2c.c
@@ -80,6 +80,34 @@ void I2C2_IRQHandler(void)
 #endif
 
 /**
+ * @brief Configure the VDDIO level for a I2C interface
+ * @param device_id - the interface number.
+ * @param vssel - the VDDIO level.
+ * @return 0 in case of success, -EINVAL otherwise.
+ */
+static int32_t _max_i2c_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
+{
+	mxc_gpio_cfg_t *i2c_pins;
+
+	switch (device_id) {
+	case 0:
+		i2c_pins = &gpio_cfg_i2c0;
+		break;
+	case 1:
+		i2c_pins = &gpio_cfg_i2c1;
+		break;
+	default:
+		return -EINVAL;
+	}
+
+	i2c_pins->vssel = vssel;
+	MXC_GPIO_Config(i2c_pins);
+
+	return 0;
+}
+
+
+/**
  * @brief Initialize the I2C communication peripheral.
  * Supported bitrates are between 100kHz and 400 kHz.
  * Is slave address is 0, then this instance will be used for general call.
@@ -93,13 +121,14 @@ void I2C2_IRQHandler(void)
 static int32_t max_i2c_init(struct no_os_i2c_desc **desc,
 			    const struct no_os_i2c_init_param *param)
 {
+	struct max_i2c_init_param *eparam;
 	int32_t ret;
 	struct max_i2c_extra *max_i2c;
 	mxc_i2c_regs_t *i2c_regs;
 	uint32_t current_freq = 0;
 	uint32_t freq;
 
-	if (!desc || !param)
+	if (!desc || !param || !param->extra)
 		return -EINVAL;
 	if (param->device_id >= MXC_I2C_INSTANCES)
 		return -EINVAL;
@@ -114,6 +143,7 @@ static int32_t max_i2c_init(struct no_os_i2c_desc **desc,
 		goto error_desc;
 	}
 
+	eparam = param->extra;
 	i2c_regs = MXC_I2C_GET_I2C(param->device_id);
 	max_i2c->handler = i2c_regs;
 	(*desc)->device_id = param->device_id;
@@ -140,6 +170,11 @@ static int32_t max_i2c_init(struct no_os_i2c_desc **desc,
 			goto error_extra;
 		}
 	}
+
+	ret = _max_i2c_pins_config((*desc)->device_id, eparam->vssel);
+	if (ret)
+		return ret;
+
 	freq = MXC_I2C_GetFrequency(i2c_regs);
 	freq = no_os_min(freq, (*desc)->max_speed_hz);
 	ret = MXC_I2C_SetFrequency(i2c_regs, freq);

commit b1e5d56df44a5d9b187aa8133c56a34d70913cba

platform: maxim: max32650: Fix reference not found
Add the header that declares the i2c pins struct.

Fixes: 25b4699e ("drivers:platform:maxim: Allow I2C vddio selection")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/platform/maxim/max32650/maxim_i2c.h
@@ -43,6 +43,7 @@
 
 #include <stdint.h>
 #include "i2c_regs.h"
+#include "mxc_pins.h"
 
 /**
  * @struct max_i2c_extra