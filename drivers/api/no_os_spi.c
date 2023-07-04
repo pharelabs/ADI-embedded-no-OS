pharelabs/ADI-embedded-no-OS

commit 80cc01030d17604ec7588bcaaff37da698dd67a9

SPI: added bus desc to api and initialization
Signed-off-by: rbudai <robert.budai@analog.com>

drivers/api/no_os_spi.c
@@ -41,6 +41,13 @@
 #include "no_os_spi.h"
 #include <stdlib.h>
 #include "no_os_error.h"
+#include "no_os_mutex.h"
+#include "no_os_alloc.h"
+
+/**
+ * @brief spi_table contains the pointers towards the SPI buses
+*/
+static void *spi_table[SPI_MAX_BUS_NUMBER + 1];
 
 /**
  * @brief Initialize the SPI communication peripheral.
@@ -58,13 +65,53 @@ int32_t no_os_spi_init(struct no_os_spi_desc **desc,
 
 	if (!param->platform_ops->init)
 		return -ENOSYS;
-
+	if (param->device_id > SPI_MAX_BUS_NUMBER)
+		return -EINVAL;
+	// Initializing BUS descriptor
+	if (!spi_table[param->device_id]) {
+		ret = no_os_spibus_init(param);
+		if (ret)
+			return ret;
+	}
+	// Initilize SPI descriptor
 	ret = param->platform_ops->init(desc, param);
 	if (ret)
 		return ret;
-
+	(*desc)->bus = spi_table[param->device_id];
 	(*desc)->platform_ops = param->platform_ops;
 	(*desc)->parent = param->parent;
+
+	return 0;
+}
+
+/**
+ * @brief Initialize the SPI bus communication peripheral.
+ * @param param - The structure that containes the SPI bus parameters
+ * @return 0 in case of success, error code otherwise
+*/
+int32_t no_os_spibus_init(const struct no_os_spi_init_param *param)
+{
+	struct no_os_spibus_desc *bus = (struct no_os_spibus_desc *)no_os_calloc(1,
+					sizeof(struct no_os_spibus_desc));
+
+	if (!bus)
+		return -ENOMEM;
+
+	no_os_mutex_init(bus->mutex);
+
+	if (!bus->mutex) {
+		no_os_free(bus);
+		return -ENOMEM;
+	}
+
+	bus->device_id = param->device_id;
+	bus->max_speed_hz = param->max_speed_hz;
+	bus->mode = param->mode;
+	bus->bit_order = param->bit_order;
+	bus->platform_ops = param->platform_ops;
+	bus->extra = param->extra;
+
+	spi_table[param->device_id] = bus;
 
 	return 0;
 }
@@ -76,13 +123,34 @@ int32_t no_os_spi_init(struct no_os_spi_desc **desc,
  */
 int32_t no_os_spi_remove(struct no_os_spi_desc *desc)
 {
+	// Remove SPI bus
+	no_os_spibus_remove(desc->device_id);
+
 	if (!desc || !desc->platform_ops)
 		return -EINVAL;
 
 	if (!desc->platform_ops->remove)
 		return -ENOSYS;
-
 	return desc->platform_ops->remove(desc);
+}
+
+/**
+ * @brief Removes SPI bus instance
+ * @param bus_number - SPI bus number
+*/
+void no_os_spibus_remove(uint32_t bus_number)
+{
+	struct no_os_spibus_desc *spi_bus_desriptor = (struct no_os_spibus_desc *)
+			spi_table[bus_number];
+
+	if (spi_bus_desriptor->mutex) {
+		no_os_mutex_remove(spi_bus_desriptor->mutex);
+		spi_bus_desriptor->mutex = 0;
+	}
+	if (spi_bus_desriptor) {
+		no_os_free(spi_bus_desriptor);
+		spi_bus_desriptor = 0;
+	}
 }
 
 /**

include/no_os_spi.h
@@ -52,6 +52,7 @@
 
 #define	NO_OS_SPI_CPHA	0x01
 #define	NO_OS_SPI_CPOL	0x02
+#define SPI_MAX_BUS_NUMBER 8
 
 /******************************************************************************/
 /*************************** Types Declarations *******************************/
@@ -137,11 +138,34 @@ struct no_os_spi_init_param {
 };
 
 /**
+ * @struct no_os_spibus_desc
+ * @brief SPI bus descriptor
+*/
+struct no_os_spibus_desc {
+	/** SPI bus mutex (lock) */
+	void 		*mutex;
+	/** SPI bus device id */
+	uint32_t	device_id;
+	/** SPI bus max speed */
+	uint32_t	max_speed_hz;
+	/** SPI bus mode */
+	enum no_os_spi_mode	mode;
+	/** SPI bus bit order */
+	enum no_os_spi_bit_order	bit_order;
+	/** SPI bus platform ops */
+	const struct no_os_spi_platform_ops *platform_ops;
+	/** SPI bus extra */
+	void		*extra;
+};
+
+/**
  * @struct no_os_spi_desc
  * @brief Structure holding SPI descriptor.
  */
 struct no_os_spi_desc {
-	/** Device ID */
+	/** SPI bus address */
+	struct no_os_spibus_desc	*bus;
+	/** SPI bus number (0 for SPI0, 1 for SPI1, ...) */
 	uint32_t	device_id;
 	/** maximum transfer speed */
 	uint32_t	max_speed_hz;
@@ -194,6 +218,12 @@ int32_t no_os_spi_write_and_read(struct no_os_spi_desc *desc,
 int32_t no_os_spi_transfer(struct no_os_spi_desc *desc,
 			   struct no_os_spi_msg *msgs,
 			   uint32_t len);
+
+/* Initialize SPI bus descriptor*/
+int32_t no_os_spibus_init(const struct no_os_spi_init_param *param);
+
+/* Free the resources allocated for SPI bus desc*/
+void no_os_spibus_remove(uint32_t bus_number);
 
 
 #endif // _NO_OS_SPI_H_

commit 26b3d6cf8a409009d9c612c243b0923bb35eaa84

api/no_os_spi.c: bug fix
without mutex initilization no check should be performed

Fixes: 80cc01030d17604ec7588bcaaff37da698dd67a9

Signed-off-by: rbudai <robert.budai@analog.com>

drivers/api/no_os_spi.c
@@ -99,11 +99,6 @@ int32_t no_os_spibus_init(const struct no_os_spi_init_param *param)
 
 	no_os_mutex_init(bus->mutex);
 
-	if (!bus->mutex) {
-		no_os_free(bus);
-		return -ENOMEM;
-	}
-
 	bus->device_id = param->device_id;
 	bus->max_speed_hz = param->max_speed_hz;
 	bus->mode = param->mode;
@@ -143,10 +138,8 @@ void no_os_spibus_remove(uint32_t bus_number)
 	struct no_os_spibus_desc *spi_bus_desriptor = (struct no_os_spibus_desc *)
 			spi_table[bus_number];
 
-	if (spi_bus_desriptor->mutex) {
-		no_os_mutex_remove(spi_bus_desriptor->mutex);
-		spi_bus_desriptor->mutex = 0;
-	}
+	no_os_mutex_remove(spi_bus_desriptor->mutex);
+
 	if (spi_bus_desriptor) {
 		no_os_free(spi_bus_desriptor);
 		spi_bus_desriptor = 0;
@@ -164,13 +157,19 @@ int32_t no_os_spi_write_and_read(struct no_os_spi_desc *desc,
 				 uint8_t *data,
 				 uint16_t bytes_number)
 {
+	int32_t ret;
+
 	if (!desc || !desc->platform_ops)
 		return -EINVAL;
 
 	if (!desc->platform_ops->write_and_read)
 		return -ENOSYS;
 
-	return desc->platform_ops->write_and_read(desc, data, bytes_number);
+	no_os_mutex_lock(desc->bus->mutex);
+	ret =  desc->platform_ops->write_and_read(desc, data, bytes_number);
+	no_os_mutex_unlock(desc->bus->mutex);
+
+	return ret;
 }
 
 /**
@@ -193,14 +192,21 @@ int32_t no_os_spi_transfer(struct no_os_spi_desc *desc,
 	if (desc->platform_ops->transfer)
 		return desc->platform_ops->transfer(desc, msgs, len);
 
+	no_os_mutex_lock(desc->bus->mutex);
+
 	for (i = 0; i < len; i++) {
-		if (msgs[i].rx_buff != msgs[i].tx_buff || !msgs[i].tx_buff)
-			return -EINVAL;
+		if (msgs[i].rx_buff != msgs[i].tx_buff || !msgs[i].tx_buff) {
+			ret = -EINVAL;
+			goto out;
+		}
 		ret = no_os_spi_write_and_read(desc, msgs[i].rx_buff,
 					       msgs[i].bytes_number);
-		if (NO_OS_IS_ERR_VALUE(ret))
-			return ret;
+		if (NO_OS_IS_ERR_VALUE(ret)) {
+			goto out;
+		}
 	}
 
+out:
+	no_os_mutex_unlock(desc->bus->mutex);
 	return 0;
 }