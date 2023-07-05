pharelabs/ADI-embedded-no-OS

commit f7245688f9aa34375edba9abc6bad22285c1026b

platform: maxim: Remove discarded const warning
The uart_pins variable has to be non const so that we may change the
vssel field. Use explicit cast when assigning it to a const struct so
that the warning is removed.

Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/platform/maxim/max32650/maxim_uart.c
@@ -78,13 +78,13 @@ static int32_t _max_uart_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
 
 	switch (device_id) {
 	case 0:
-		uart_pins = &gpio_cfg_uart0;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart0;
 		break;
 	case 1:
-		uart_pins = &gpio_cfg_uart1;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart1;
 		break;
 	case 2:
-		uart_pins = &gpio_cfg_uart2;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart2;
 		break;
 	default:
 		return -EINVAL;

drivers/platform/maxim/max32655/maxim_uart.c
@@ -88,13 +88,13 @@ static int32_t _max_uart_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
 
 	switch (device_id) {
 	case 0:
-		uart_pins = &gpio_cfg_uart0;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart0;
 		break;
 	case 1:
-		uart_pins = &gpio_cfg_uart1;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart1;
 		break;
 	case 2:
-		uart_pins = &gpio_cfg_uart2;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart2;
 		break;
 	default:
 		return -EINVAL;

drivers/platform/maxim/max32660/maxim_uart.c
@@ -78,10 +78,10 @@ static int32_t _max_uart_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
 
 	switch (device_id) {
 	case 0:
-		uart_pins = &gpio_cfg_uart0;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart0;
 		break;
 	case 1:
-		uart_pins = &gpio_cfg_uart1a;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart1a;
 		break;
 	default:
 		return -EINVAL;

drivers/platform/maxim/max32665/maxim_uart.c
@@ -88,13 +88,13 @@ static int32_t _max_uart_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
 
 	switch (device_id) {
 	case 0:
-		uart_pins = &gpio_cfg_uart0b;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart0b;
 		break;
 	case 1:
-		uart_pins = &gpio_cfg_uart1b;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart1b;
 		break;
 	case 2:
-		uart_pins = &gpio_cfg_uart2b;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart2b;
 		break;
 	default:
 		return -EINVAL;

drivers/platform/maxim/max78000/maxim_uart.c
@@ -91,16 +91,16 @@ static int32_t _max_uart_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
 
 	switch (device_id) {
 	case 0:
-		uart_pins = &gpio_cfg_uart0;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart0;
 		break;
 	case 1:
-		uart_pins = &gpio_cfg_uart1;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart1;
 		break;
 	case 2:
-		uart_pins = &gpio_cfg_uart2;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart2;
 		break;
 	case 3:
-		uart_pins = &gpio_cfg_uart3;
+		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart3;
 		break;
 	default:
 		return -EINVAL;

commit 9dafa47a1974212614c56ae9a6b281032eb95b34

drivers:platform:maxim: Fix UART UB
Currently, when setting the voltage level of the UART interface, it is
done by changing the vssel field of a struct defined in the SDK (which
is used to configure some GPIOs as UART alternate function). However,
these structs are defined as const, and as such, modifying it's fields
is undefined behavior.

The initialization sequence is still correct, since the voltage level is
set after the UART interface is configured.

Fixes: f724568 ("platform: maxim: Remove discarded const warning")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/platform/maxim/max32650/maxim_uart.c
@@ -74,24 +74,24 @@ static uint8_t c;
  */
 static int32_t _max_uart_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
 {
-	mxc_gpio_cfg_t *uart_pins;
+	mxc_gpio_cfg_t uart_pins;
 
 	switch (device_id) {
 	case 0:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart0;
+		uart_pins = gpio_cfg_uart0;
 		break;
 	case 1:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart1;
+		uart_pins = gpio_cfg_uart1;
 		break;
 	case 2:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart2;
+		uart_pins = gpio_cfg_uart2;
 		break;
 	default:
 		return -EINVAL;
 	}
 
-	uart_pins->vssel = vssel;
-	MXC_GPIO_Config(uart_pins);
+	uart_pins.vssel = vssel;
+	MXC_GPIO_Config(&uart_pins);
 
 	return 0;
 }

drivers/platform/maxim/max32655/maxim_uart.c
@@ -84,24 +84,24 @@ static void _discard_callback(mxc_uart_req_t *req, int result)
  */
 static int32_t _max_uart_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
 {
-	mxc_gpio_cfg_t *uart_pins;
+	mxc_gpio_cfg_t uart_pins;
 
 	switch (device_id) {
 	case 0:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart0;
+		uart_pins = gpio_cfg_uart0;
 		break;
 	case 1:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart1;
+		uart_pins = gpio_cfg_uart1;
 		break;
 	case 2:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart2;
+		uart_pins = gpio_cfg_uart2;
 		break;
 	default:
 		return -EINVAL;
 	}
 
-	uart_pins->vssel = vssel;
-	MXC_GPIO_Config(uart_pins);
+	uart_pins.vssel = vssel;
+	MXC_GPIO_Config(&uart_pins);
 
 	return 0;
 }

drivers/platform/maxim/max32660/maxim_uart.c
@@ -74,21 +74,21 @@ static uint8_t c;
  */
 static int32_t _max_uart_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
 {
-	mxc_gpio_cfg_t *uart_pins;
+	mxc_gpio_cfg_t uart_pins;
 
 	switch (device_id) {
 	case 0:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart0;
+		uart_pins = gpio_cfg_uart0;
 		break;
 	case 1:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart1a;
+		uart_pins = gpio_cfg_uart1a;
 		break;
 	default:
 		return -EINVAL;
 	}
 
-	uart_pins->vssel = vssel;
-	MXC_GPIO_Config(uart_pins);
+	uart_pins.vssel = vssel;
+	MXC_GPIO_Config(&uart_pins);
 
 	return 0;
 }

drivers/platform/maxim/max32665/maxim_uart.c
@@ -84,24 +84,24 @@ static void _discard_callback(mxc_uart_req_t *req, int result)
  */
 static int32_t _max_uart_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
 {
-	mxc_gpio_cfg_t *uart_pins;
+	mxc_gpio_cfg_t uart_pins;
 
 	switch (device_id) {
 	case 0:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart0b;
+		uart_pins = gpio_cfg_uart0b;
 		break;
 	case 1:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart1b;
+		uart_pins = gpio_cfg_uart1b;
 		break;
 	case 2:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart2b;
+		uart_pins = gpio_cfg_uart2b;
 		break;
 	default:
 		return -EINVAL;
 	}
 
-	uart_pins->vssel = vssel;
-	MXC_GPIO_Config(uart_pins);
+	uart_pins.vssel = vssel;
+	MXC_GPIO_Config(&uart_pins);
 
 	return 0;
 }

drivers/platform/maxim/max32690/maxim_uart.c
@@ -76,27 +76,27 @@ static void _discard_callback(mxc_uart_req_t *req, int result)
  */
 static int32_t _max_uart_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
 {
-	mxc_gpio_cfg_t *uart_pins;
+	mxc_gpio_cfg_t uart_pins;
 
 	switch (device_id) {
 	case 0:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart0;
+		uart_pins = gpio_cfg_uart0;
 		break;
 	case 1:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart1;
+		uart_pins = gpio_cfg_uart1;
 		break;
 	case 2:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart2;
+		uart_pins = gpio_cfg_uart2;
 		break;
 	case 3:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart3;
+		uart_pins = gpio_cfg_uart3;
 		break;
 	default:
 		return -EINVAL;
 	}
 
-	uart_pins->vssel = vssel;
-	MXC_GPIO_Config(uart_pins);
+	uart_pins.vssel = vssel;
+	MXC_GPIO_Config(&uart_pins);
 
 	return 0;
 }

drivers/platform/maxim/max78000/maxim_uart.c
@@ -87,27 +87,27 @@ static void _discard_callback(mxc_uart_req_t *req, int result)
  */
 static int32_t _max_uart_pins_config(uint32_t device_id, mxc_gpio_vssel_t vssel)
 {
-	mxc_gpio_cfg_t *uart_pins;
+	mxc_gpio_cfg_t uart_pins;
 
 	switch (device_id) {
 	case 0:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart0;
+		uart_pins = gpio_cfg_uart0;
 		break;
 	case 1:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart1;
+		uart_pins = gpio_cfg_uart1;
 		break;
 	case 2:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart2;
+		uart_pins = gpio_cfg_uart2;
 		break;
 	case 3:
-		uart_pins = (mxc_gpio_cfg_t *)&gpio_cfg_uart3;
+		uart_pins = gpio_cfg_uart3;
 		break;
 	default:
 		return -EINVAL;
 	}
 
-	uart_pins->vssel = vssel;
-	MXC_GPIO_Config(uart_pins);
+	uart_pins.vssel = vssel;
+	MXC_GPIO_Config(&uart_pins);
 
 	return 0;
 }