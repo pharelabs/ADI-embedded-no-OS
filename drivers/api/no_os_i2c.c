pharelabs/ADI-embedded-no-OS

commit 3ce6ff1cc7179f106a513ca58afdc6f94215337b

spi and i2c: added slave counter for deallocation check
Slave counter fields were added to the bus descriptors that
help the memory deallocation check. They don't allow the
slave bus descriptor deallocation while there are slaves
counted by these fields.

Signed-off-by: rbudai <robert.budai@analog.com>

drivers/api/no_os_i2c.c
@@ -78,6 +78,7 @@ int32_t no_os_i2c_init(struct no_os_i2c_desc **desc,
 	if (ret)
 		return ret;
 	(*desc)->bus = i2c_table[param->device_id];
+	(*desc)->bus->slave_number++;
 	(*desc)->platform_ops = param->platform_ops;
 
 	return 0;
@@ -98,6 +99,7 @@ int32_t no_os_i2cbus_init(const struct no_os_i2c_init_param *param)
 
 	no_os_mutex_init(&(bus->mutex));
 
+	bus->slave_number = 0;
 	bus->device_id = param->device_id;
 	bus->max_speed_hz = param->max_speed_hz;
 	bus->platform_ops = param->platform_ops;
@@ -117,7 +119,7 @@ int32_t no_os_i2cbus_init(const struct no_os_i2c_init_param *param)
 int32_t no_os_i2c_remove(struct no_os_i2c_desc *desc)
 {
 	// Remove I2C bus
-	no_os_i2cbus_remove(desc->device_id);
+	no_os_i2cbus_remove(desc->bus->device_id);
 
 	if (!desc || !desc->platform_ops)
 		return -EINVAL;
@@ -134,14 +136,19 @@ int32_t no_os_i2c_remove(struct no_os_i2c_desc *desc)
 */
 void no_os_i2cbus_remove(uint32_t bus_number)
 {
-	struct no_os_i2cbus_desc *i2c_bus_desriptor = (struct no_os_i2cbus_desc *)
-			i2c_table[bus_number];
+	struct no_os_i2cbus_desc *bus = (struct no_os_i2cbus_desc *)
+					i2c_table[bus_number];
 
-	no_os_mutex_remove(i2c_bus_desriptor->mutex);
+	if (bus->slave_number > 0)
+		bus->slave_number--;
 
-	if (i2c_bus_desriptor != NULL) {
-		no_os_free(i2c_bus_desriptor);
-		i2c_bus_desriptor = NULL;
+	if (bus->slave_number == 0) {
+		no_os_mutex_remove(bus->mutex);
+
+		if (bus != NULL) {
+			no_os_free(bus);
+			bus = NULL;
+		}
 	}
 }
 

drivers/api/no_os_spi.c
@@ -78,6 +78,7 @@ int32_t no_os_spi_init(struct no_os_spi_desc **desc,
 	if (ret)
 		return ret;
 	(*desc)->bus = spi_table[param->device_id];
+	(*desc)->bus->slave_number++;
 	(*desc)->platform_ops = param->platform_ops;
 	(*desc)->parent = param->parent;
 
@@ -99,6 +100,7 @@ int32_t no_os_spibus_init(const struct no_os_spi_init_param *param)
 
 	no_os_mutex_init(&(bus->mutex));
 
+	bus->slave_number = 0;
 	bus->device_id = param->device_id;
 	bus->max_speed_hz = param->max_speed_hz;
 	bus->mode = param->mode;
@@ -119,7 +121,7 @@ int32_t no_os_spibus_init(const struct no_os_spi_init_param *param)
 int32_t no_os_spi_remove(struct no_os_spi_desc *desc)
 {
 	// Remove SPI bus
-	no_os_spibus_remove(desc->device_id);
+	no_os_spibus_remove(desc->bus->device_id);
 
 	if (!desc || !desc->platform_ops)
 		return -EINVAL;
@@ -135,14 +137,19 @@ int32_t no_os_spi_remove(struct no_os_spi_desc *desc)
 */
 void no_os_spibus_remove(uint32_t bus_number)
 {
-	struct no_os_spibus_desc *spi_bus_desriptor = (struct no_os_spibus_desc *)
-			spi_table[bus_number];
+	struct no_os_spibus_desc *bus = (struct no_os_spibus_desc *)
+					spi_table[bus_number];
 
-	no_os_mutex_remove(spi_bus_desriptor->mutex);
+	if (bus->slave_number > 0)
+		bus->slave_number--;
 
-	if (spi_bus_desriptor) {
-		no_os_free(spi_bus_desriptor);
-		spi_bus_desriptor = 0;
+	if (bus->slave_number == 0) {
+		no_os_mutex_remove(bus->mutex);
+
+		if (bus) {
+			no_os_free(bus);
+			bus = NULL;
+		}
 	}
 }
 

include/no_os_i2c.h
@@ -87,6 +87,8 @@ struct no_os_i2c_init_param {
 struct no_os_i2cbus_desc {
 	/** I2C bus mutex(lock)*/
 	void* mutex;
+	/** I2C bus slave number*/
+	uint8_t         slave_number;
 	/** I2C bus Device ID */
 	uint32_t	device_id;
 	/** I2C bus maximum transfer speed supported */

include/no_os_spi.h
@@ -144,6 +144,8 @@ struct no_os_spi_init_param {
 struct no_os_spibus_desc {
 	/** SPI bus mutex (lock) */
 	void 		*mutex;
+	/** SPI bus slave number*/
+	uint8_t         slave_number;
 	/** SPI bus device id */
 	uint32_t	device_id;
 	/** SPI bus max speed */

commit a8c7cab49c7d812be18ff36a5d5057845f7db1d5

drivers: api: Fix variable assignment
The intent is to set i2c_table[bus_number] to NULL, so that we know the
memory has been freed. However, the current implementation just makes
the bus variable point to NULL, while i2c_table[bus_number] remains
unchaged.

Running the init(), remove(), init() sequence will result in using freed
memory after the second init. Fix this by correctly setting the
i2c_table array element to NULL.

Fixes: 3ce6ff1 ("spi and i2c: added slave counter for deallocation check")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/api/no_os_i2c.c
@@ -147,7 +147,7 @@ void no_os_i2cbus_remove(uint32_t bus_number)
 
 		if (bus != NULL) {
 			no_os_free(bus);
-			bus = NULL;
+			i2c_table[bus_number] = NULL;
 		}
 	}
 }