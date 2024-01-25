pharelabs/ADI-embedded-no-OS

commit ac07b6a0187ce43113a2c57afa30e4c5685795ea

I2C: added bus desc to api and initialization
Signed-off-by: rbudai <robert.budai@analog.com>

drivers/api/no_os_i2c.c
@@ -41,6 +41,13 @@
 #include "no_os_i2c.h"
 #include <stdlib.h>
 #include "no_os_error.h"
+#include "no_os_mutex.h"
+#include "no_os_alloc.h"
+
+/**
+ * @brief i2c_table contains the pointers towards the i2c buses
+*/
+static void *i2c_table[I2C_MAX_BUS_NUMBER + 1];
 
 /**
  * @brief Initialize the I2C communication peripheral.
@@ -58,15 +65,54 @@ int32_t no_os_i2c_init(struct no_os_i2c_desc **desc,
 
 	if (!param->platform_ops->i2c_ops_init)
 		return -ENOSYS;
-
+	if ( param->device_id > I2C_MAX_BUS_NUMBER)
+		return -EINVAL;
+	// Initializing BUS descriptor
+	if (i2c_table[param->device_id] == NULL) {
+		ret = no_os_i2cbus_init(param);
+		if (ret)
+			return ret;
+	}
+	// Initilize I2C descriptor
 	ret = param->platform_ops->i2c_ops_init(desc, param);
 	if (ret)
 		return ret;
-
+	(*desc)->bus = i2c_table[param->device_id];
 	(*desc)->platform_ops = param->platform_ops;
 
 	return 0;
 }
+
+/**
+ * @brief Initialize the i2c bus communication peripheral.
+ * @param param - The structure that containes the i2c bus parameters
+ * @return 0 in case of success, error code otherwise
+*/
+int32_t no_os_i2cbus_init(const struct no_os_i2c_init_param *param)
+{
+	struct no_os_i2cbus_desc *bus = (struct no_os_i2cbus_desc *)no_os_calloc(1,
+					sizeof(struct no_os_i2cbus_desc));
+
+	if (!bus)
+		return -ENOMEM;
+
+	no_os_mutex_init(bus->mutex);
+
+	if (bus->mutex == NULL) {
+		no_os_free(bus);
+		return -ENOMEM;
+	}
+
+	bus->device_id = param->device_id;
+	bus->max_speed_hz = param->max_speed_hz;
+	bus->platform_ops = param->platform_ops;
+	bus->extra = param->extra;
+
+	i2c_table[param->device_id] = bus;
+
+	return 0;
+}
+
 
 /**
  * @brief Free the resources allocated by no_os_i2c_init().
@@ -75,6 +121,9 @@ int32_t no_os_i2c_init(struct no_os_i2c_desc **desc,
  */
 int32_t no_os_i2c_remove(struct no_os_i2c_desc *desc)
 {
+	// Remove I2C bus
+	no_os_i2cbus_remove(desc->device_id);
+
 	if (!desc || !desc->platform_ops)
 		return -EINVAL;
 
@@ -82,6 +131,25 @@ int32_t no_os_i2c_remove(struct no_os_i2c_desc *desc)
 		return -ENOSYS;
 
 	return desc->platform_ops->i2c_ops_remove(desc);
+}
+
+/**
+ * @brief Removes i2c bus instance
+ * @param bus_number - i2c bus number
+*/
+void no_os_i2cbus_remove(uint32_t bus_number)
+{
+	struct no_os_i2cbus_desc *i2c_bus_desriptor = (struct no_os_i2cbus_desc *)
+			i2c_table[bus_number];
+
+	if (i2c_bus_desriptor->mutex != NULL) {
+		no_os_mutex_remove(i2c_bus_desriptor->mutex);
+		i2c_bus_desriptor->mutex = NULL;
+	}
+	if (i2c_bus_desriptor != NULL) {
+		no_os_free(i2c_bus_desriptor);
+		i2c_bus_desriptor = NULL;
+	}
 }
 
 /**

include/no_os_i2c.h
@@ -47,6 +47,12 @@
 #include <stdint.h>
 
 /******************************************************************************/
+/********************** Macros and Constants Definitions **********************/
+/******************************************************************************/
+
+#define I2C_MAX_BUS_NUMBER 4
+
+/******************************************************************************/
 /*************************** Types Declarations *******************************/
 /******************************************************************************/
 
@@ -75,10 +81,30 @@ struct no_os_i2c_init_param {
 };
 
 /**
+ * @struct no_os_i2cbus_desc
+ * @brief Structure holding I2C bus descriptor
+ */
+struct no_os_i2cbus_desc {
+	/** I2C bus mutex(lock)*/
+	void* mutex;
+	/** I2C bus Device ID */
+	uint32_t	device_id;
+	/** I2C bus maximum transfer speed supported */
+	uint32_t	max_speed_hz;
+	/** I2C bus platform specific functions */
+	const struct no_os_i2c_platform_ops *platform_ops;
+	/** I2C bus extra parameters (device specific parameters) */
+	void		*extra;
+};
+
+
+/**
  * @struct no_os_i2c_desc
- * @brief Structure holding I2C descriptor
+ * @brief Structure holding I2C address descriptor
  */
 struct no_os_i2c_desc {
+	/** I2C bus address*/
+	struct no_os_i2cbus_desc *bus;
 	/** Device ID */
 	uint32_t	device_id;
 	/** I2C maximum transfer speed supported */
@@ -130,5 +156,11 @@ int32_t no_os_i2c_read(struct no_os_i2c_desc *desc,
 		       uint8_t *data,
 		       uint8_t bytes_number,
 		       uint8_t stop_bit);
+
+/* Initialize I2C bus descriptor*/
+int32_t no_os_i2cbus_init(const struct no_os_i2c_init_param *param);
+
+/* Free the resources allocated for I2C  bus desc*/
+void no_os_i2cbus_remove(uint32_t bus_number);
 
 #endif // _NO_OS_I2C_H_

commit 1b7453cad6219eab66cd844cd552bb0cac10210d

drivers: api: no_os_i2c: Fix remove API
Call i2cbus_remove only if desc and desc-> bus are not null to
avoid undefined behavior.

Fixes: ac07b6a ("I2C: added bus desc to api and initialization")
Signed-off-by: Ramona Gradinariu <ramona.gradinariu@analog.com>

drivers/api/no_os_i2c.c
@@ -118,11 +118,11 @@ int32_t no_os_i2cbus_init(const struct no_os_i2c_init_param *param)
  */
 int32_t no_os_i2c_remove(struct no_os_i2c_desc *desc)
 {
-	// Remove I2C bus
-	no_os_i2cbus_remove(desc->bus->device_id);
-
 	if (!desc || !desc->platform_ops)
 		return -EINVAL;
+
+	if (desc->bus)
+		no_os_i2cbus_remove(desc->bus->device_id);
 
 	if (!desc->platform_ops->i2c_ops_remove)
 		return -ENOSYS;