pharelabs/ADI-embedded-no-OS

commit e291e8531b11a5aee4de991cf70df02276e9482b

drivers: platform: maxim: Fix irq_desc removal
Currently, there may be multiple interrupt controllers allocated,
even though there is only one hardware instance (NVIC). The issue
is that is no_os_irq_ctrl_remove() is called for an interrupt
controller with ref = 0, all the callback functions will be
removed, even those which were registered using another controller.

Fix this by turning the interrupt controller into a shared reference,
thus max_irq_ctrl_remove() will only be called once ref = 0.

Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/platform/maxim/max32650/maxim_irq.c
@@ -72,6 +72,7 @@ static struct event_list _events[] = {
 	[NO_OS_EVT_USB] = {.event = NO_OS_EVT_USB},
 };
 
+static struct no_os_irq_ctrl_desc *nvic;
 extern mxc_uart_req_t uart_irq_state[MXC_UART_INSTANCES];
 extern bool is_callback;
 
@@ -386,6 +387,11 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	if (!param)
 		return -EINVAL;
 
+	if (nvic) {
+		descriptor = nvic;
+		return 0;
+	}
+
 	descriptor = no_os_calloc(1, sizeof(*descriptor));
 	if (!descriptor)
 		return -ENOMEM;
@@ -394,6 +400,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	descriptor->extra = param->extra;
 
 	*desc = descriptor;
+	nvic = descriptor;
 
 	return 0;
 }
@@ -415,6 +422,7 @@ int32_t max_irq_ctrl_remove(struct no_os_irq_ctrl_desc *desc)
 		no_os_list_remove(_events[i].actions);
 	}
 	no_os_free(desc);
+	nvic = NULL;
 
 	return 0;
 }

drivers/platform/maxim/max32655/maxim_irq.c
@@ -70,6 +70,7 @@ static struct event_list _events[] = {
 	[NO_OS_EVT_DMA_TX_COMPLETE] = {.event = NO_OS_EVT_DMA_TX_COMPLETE},
 };
 
+static struct no_os_irq_ctrl_desc *nvic;
 extern mxc_uart_req_t uart_irq_state[MXC_UART_INSTANCES];
 extern bool is_callback;
 
@@ -369,6 +370,11 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	if (!param)
 		return -EINVAL;
 
+	if (nvic) {
+		descriptor = nvic;
+		return 0;
+	}
+
 	descriptor = no_os_calloc(1, sizeof(*descriptor));
 	if (!descriptor)
 		return -ENOMEM;
@@ -377,6 +383,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	descriptor->extra = param->extra;
 
 	*desc = descriptor;
+	nvic = descriptor;
 
 	return 0;
 }
@@ -399,6 +406,7 @@ int32_t max_irq_ctrl_remove(struct no_os_irq_ctrl_desc *desc)
 		_events[i].actions = NULL;
 	}
 	no_os_free(desc);
+	nvic = NULL;
 
 	return 0;
 }

drivers/platform/maxim/max32660/maxim_irq.c
@@ -70,6 +70,7 @@ static struct event_list _events[] = {
 	[NO_OS_EVT_DMA_TX_COMPLETE] = {.event = NO_OS_EVT_DMA_TX_COMPLETE},
 };
 
+static struct no_os_irq_ctrl_desc *nvic;
 extern mxc_uart_req_t uart_irq_state[MXC_UART_INSTANCES];
 extern bool is_callback;
 
@@ -369,6 +370,11 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	if (!param)
 		return -EINVAL;
 
+	if (nvic) {
+		descriptor = nvic;
+		return 0;
+	}
+
 	descriptor = no_os_calloc(1, sizeof(*descriptor));
 	if (!descriptor)
 		return -ENOMEM;
@@ -377,6 +383,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	descriptor->extra = param->extra;
 
 	*desc = descriptor;
+	nvic = descriptor;
 
 	return 0;
 }
@@ -399,6 +406,7 @@ int32_t max_irq_ctrl_remove(struct no_os_irq_ctrl_desc *desc)
 		_events[i].actions = NULL;
 	}
 	no_os_free(desc);
+	nvic = NULL;
 
 	return 0;
 }

drivers/platform/maxim/max32665/maxim_irq.c
@@ -71,6 +71,7 @@ static struct event_list _events[] = {
 	[NO_OS_EVT_USB] = {.event = NO_OS_EVT_USB},
 };
 
+static struct no_os_irq_ctrl_desc *nvic;
 extern mxc_uart_req_t uart_irq_state[MXC_UART_INSTANCES];
 extern bool is_callback;
 
@@ -390,6 +391,11 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	if (!param)
 		return -EINVAL;
 
+	if (nvic) {
+		descriptor = nvic;
+		return 0;
+	}
+
 	descriptor = no_os_calloc(1, sizeof(*descriptor));
 	if (!descriptor)
 		return -ENOMEM;
@@ -398,6 +404,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	descriptor->extra = param->extra;
 
 	*desc = descriptor;
+	nvic = descriptor;
 
 	return 0;
 }
@@ -420,6 +427,7 @@ int32_t max_irq_ctrl_remove(struct no_os_irq_ctrl_desc *desc)
 		_events[i].actions = NULL;
 	}
 	no_os_free(desc);
+	nvic = NULL;
 
 	return 0;
 }

drivers/platform/maxim/max32670/maxim_irq.c
@@ -63,6 +63,7 @@ static struct event_list _events[] = {
 	[NO_OS_EVT_TIM_ELAPSED] = {.event = NO_OS_EVT_TIM_ELAPSED},
 };
 
+static struct no_os_irq_ctrl_desc *nvic;
 extern mxc_uart_req_t uart_irq_state[MXC_UART_INSTANCES];
 extern bool is_callback;
 
@@ -253,6 +254,11 @@ int max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	if (!param)
 		return -EINVAL;
 
+	if (nvic) {
+		descriptor = nvic;
+		return 0;
+	}
+
 	descriptor = no_os_calloc(1, sizeof(*descriptor));
 	if (!descriptor)
 		return -ENOMEM;
@@ -261,6 +267,7 @@ int max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	descriptor->extra = param->extra;
 
 	*desc = descriptor;
+	nvic = descriptor;
 
 	return 0;
 }
@@ -284,6 +291,7 @@ int max_irq_ctrl_remove(struct no_os_irq_ctrl_desc *desc)
 		_events[i].actions = NULL;
 	}
 	no_os_free(desc);
+	nvic = NULL;
 
 	return 0;
 }

drivers/platform/maxim/max32690/maxim_irq.c
@@ -67,6 +67,7 @@ static struct event_list _events[] = {
 	[NO_OS_EVT_USB] = {.event = NO_OS_EVT_USB},
 };
 
+static struct no_os_irq_ctrl_desc *nvic;
 extern mxc_uart_req_t uart_irq_state[MXC_UART_INSTANCES];
 extern bool is_callback;
 
@@ -377,6 +378,11 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	if (!param)
 		return -EINVAL;
 
+	if (nvic) {
+		descriptor = nvic;
+		return 0;
+	}
+
 	descriptor = no_os_calloc(1, sizeof(*descriptor));
 	if (!descriptor)
 		return -ENOMEM;
@@ -385,6 +391,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	descriptor->extra = param->extra;
 
 	*desc = descriptor;
+	nvic = descriptor;
 
 	return 0;
 }
@@ -407,6 +414,7 @@ int32_t max_irq_ctrl_remove(struct no_os_irq_ctrl_desc *desc)
 		_events[i].actions = NULL;
 	}
 	free(desc);
+	nvic = NULL;
 
 	return 0;
 }

drivers/platform/maxim/max78000/maxim_irq.c
@@ -70,6 +70,7 @@ static struct event_list _events[] = {
 	[NO_OS_EVT_DMA_TX_COMPLETE] = {.event = NO_OS_EVT_DMA_TX_COMPLETE},
 };
 
+static struct no_os_irq_ctrl_desc *nvic;
 extern mxc_uart_req_t uart_irq_state[MXC_UART_INSTANCES];
 extern bool is_callback;
 
@@ -369,6 +370,11 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	if (!param)
 		return -EINVAL;
 
+	if (nvic) {
+		descriptor = nvic;
+		return 0;
+	}
+
 	descriptor = no_os_calloc(1, sizeof(*descriptor));
 	if (!descriptor)
 		return -ENOMEM;
@@ -377,6 +383,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 	descriptor->extra = param->extra;
 
 	*desc = descriptor;
+	nvic = descriptor;
 
 	return 0;
 }
@@ -399,6 +406,7 @@ int32_t max_irq_ctrl_remove(struct no_os_irq_ctrl_desc *desc)
 		_events[i].actions = NULL;
 	}
 	no_os_free(desc);
+	nvic = NULL;
 
 	return 0;
 }

commit cbc2f7ff3d64a49bafe19bb54d122226bc196b59

platform: drivers: maxim: Assign the proper irq_ctrl_desc
Assign the user's no_os_irq_ctrl_desc instead of the local one.

Fixes: e291e8531 ("drivers: platform: maxim: Fix irq_desc removal")
Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/platform/maxim/max32650/maxim_irq.c
@@ -388,7 +388,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 		return -EINVAL;
 
 	if (nvic) {
-		descriptor = nvic;
+		*desc = nvic;
 		return 0;
 	}
 

drivers/platform/maxim/max32655/maxim_irq.c
@@ -371,7 +371,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 		return -EINVAL;
 
 	if (nvic) {
-		descriptor = nvic;
+		*desc = nvic;
 		return 0;
 	}
 

drivers/platform/maxim/max32660/maxim_irq.c
@@ -371,7 +371,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 		return -EINVAL;
 
 	if (nvic) {
-		descriptor = nvic;
+		*desc = nvic;
 		return 0;
 	}
 

drivers/platform/maxim/max32665/maxim_irq.c
@@ -392,7 +392,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 		return -EINVAL;
 
 	if (nvic) {
-		descriptor = nvic;
+		*desc = nvic;
 		return 0;
 	}
 

drivers/platform/maxim/max32670/maxim_irq.c
@@ -255,7 +255,7 @@ int max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 		return -EINVAL;
 
 	if (nvic) {
-		descriptor = nvic;
+		*desc = nvic;
 		return 0;
 	}
 

drivers/platform/maxim/max32690/maxim_irq.c
@@ -379,7 +379,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 		return -EINVAL;
 
 	if (nvic) {
-		descriptor = nvic;
+		*desc = nvic;
 		return 0;
 	}
 

drivers/platform/maxim/max78000/maxim_irq.c
@@ -371,7 +371,7 @@ int32_t max_irq_ctrl_init(struct no_os_irq_ctrl_desc **desc,
 		return -EINVAL;
 
 	if (nvic) {
-		descriptor = nvic;
+		*desc = nvic;
 		return 0;
 	}
 