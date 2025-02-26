analogdevicesinc/no-OS

drivers: spi: Add SPI Abort API
Add Abort API to abort SPI transactions.

Signed-off-by: Naga Himanshu Indraganti <naga.indraganti@analog.com>

drivers/api/no_os_spi.c
@@ -260,3 +260,19 @@ int32_t no_os_spi_transfer_dma_async(struct no_os_spi_desc *desc,
 
 	return -ENOSYS;
 }
+
+/**
+ * @brief Abort SPI transfers.
+ * @param desc - The SPI descriptor.
+ * @return 0 in case of success, -1 otherwise.
+ */
+int32_t no_os_spi_transfer_abort(struct no_os_spi_desc *desc)
+{
+	if (!desc || !desc->platform_ops)
+		return -EINVAL;
+
+	if (!desc->platform_ops->abort_transfer)
+		return -ENOSYS;
+
+	return desc->platform_ops->abort_transfer(desc);
+}

include/no_os_spi.h
@@ -239,6 +239,8 @@ struct no_os_spi_platform_ops {
 				      uint32_t, void (*)(void *), void *);
 	/** SPI remove function pointer */
 	int32_t (*remove)(struct no_os_spi_desc *);
+	/** SPI abort function pointer */
+	int32_t (*abort_transfer)(struct no_os_spi_desc *);
 };
 
 /******************************************************************************/
@@ -275,6 +277,9 @@ int32_t no_os_spi_transfer_dma_async(struct no_os_spi_desc *desc,
 				     uint32_t len,
 				     void (*callback)(void *),
 				     void *ctx);
+
+/* Abort SPI transfers. */
+int32_t no_os_spi_transfer_abort(struct no_os_spi_desc *desc);
 
 /* Initialize SPI bus descriptor*/
 int32_t no_os_spibus_init(const struct no_os_spi_init_param *param);

pharelabs/ADI-embedded-no-OS

drivers/api/no_os_spi.c
@@ -260,3 +260,19 @@ int32_t no_os_spi_transfer_dma_async(struct no_os_spi_desc *desc,
 
 	return -ENOSYS;
 }
+
+/**
+ * @brief Abort SPI transfers.
+ * @param desc - The SPI descriptor.
+ * @return 0 in case of success, -1 otherwise.
+ */
+int32_t no_os_spi_transfer_abort(struct no_os_spi_desc *desc)
+{
+	if (!desc || !desc->platform_ops)
+		return -EINVAL;
+
+	if (!desc->platform_ops->transfer_abort)
+		return -ENOSYS;
+
+	return desc->platform_ops->transfer_abort(desc);
+}

include/no_os_spi.h
@@ -227,6 +227,8 @@ struct no_os_spi_platform_ops {
 				      uint32_t, void (*)(void *), void *);
 	/** SPI remove function pointer */
 	int32_t (*remove)(struct no_os_spi_desc *);
+	/** SPI abort function pointer */
+	int32_t (*transfer_abort)(struct no_os_spi_desc *);
 };
 
 /* Initialize the SPI communication peripheral. */
@@ -259,6 +261,9 @@ int32_t no_os_spi_transfer_dma_async(struct no_os_spi_desc *desc,
 				     uint32_t len,
 				     void (*callback)(void *),
 				     void *ctx);
+
+/* Abort SPI transfers. */
+int32_t no_os_spi_transfer_abort(struct no_os_spi_desc *desc);
 
 /* Initialize SPI bus descriptor*/
 int32_t no_os_spibus_init(const struct no_os_spi_init_param *param);