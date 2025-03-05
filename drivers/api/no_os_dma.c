pharelabs/ADI-embedded-no-OS

commit bea3b2a68bbebeac8c87aa9e02c782c57f55d8b6

Add a generic DMA API
Add a DMA API, which is meant to handle scatter-gather memory to memory,
memory to peripheral and peripheral to memory transfers.

This layer abstracts a platform's DMA as one or more DMA controllers,
each having one or more DMA channels (which are not shared between
controllers). Each channel has a list of transfers, which by default are
cycled between using an end of transfer interrupt handler, that is also
implemented as part of the generic DMA layer. In case the interrupt
mechanism is different for a particular DMA IP, the handling has to be
done by accessing the list in order to enqueue or dequeue transfers.

Signed-off-by: Ciprian Regus <ciprian.regus@analog.com>

drivers/api/no_os_dma.c
@@ -1,0 +1,452 @@
+/***************************************************************************//**
+ *   @file   no_os_dma.c
+ *   @brief  Platform independent implementation for the DMA API.
+ *   @author Ciprian Regus (ciprian.regus@analog.com)
+********************************************************************************
+ * Copyright 2024(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#include <inttypes.h>
+#include "no_os_dma.h"
+#include <stdlib.h>
+#include "no_os_error.h"
+#include "no_os_mutex.h"
+#include "no_os_irq.h"
+#include "no_os_alloc.h"
+#include "no_os_list.h"
+
+/**
+ * @brief Default handler for cycling though the channel's list of transfers
+ * @param context - structure which stores the state of the current channel
+ * 		    and DMA controller state.
+ */
+static void default_sg_callback(void *context)
+{
+	struct no_os_dma_default_handler_data *data = context;
+	struct no_os_dma_xfer_desc *next_xfer;
+	struct no_os_dma_xfer_desc *old_xfer;
+	uint32_t list_size;
+	int ret;
+
+	/* Handle the next transfer from the SG list */
+	ret = no_os_list_get_first(data->channel->sg_list, (void **)&old_xfer);
+	if (ret) {
+		/*
+		 * This should only happen if the list descriptor is NULL.
+		 * The case in which there is no transfer left in the list should
+		 * have been handled in the previous interrupt.
+		 */
+		no_os_dma_xfer_abort(data->desc, data->channel);
+		return;
+	}
+
+	no_os_list_read_first(data->channel->sg_list, (void **)&next_xfer);
+	no_os_list_get_size(data->channel->sg_list, &list_size);
+	if (old_xfer->xfer_complete_cb)
+		old_xfer->xfer_complete_cb(old_xfer, next_xfer,
+					   old_xfer->xfer_complete_ctx);
+
+	if (!list_size) {
+		no_os_irq_disable(data->desc->irq_ctrl, data->channel->irq_num);
+		data->channel->free = true;
+		return;
+	}
+
+	data->desc->platform_ops->dma_config_xfer(data->channel, next_xfer);
+
+	no_os_dma_xfer_start(data->desc, data->channel);
+}
+
+/**
+ * @brief Initialize the DMA controller.
+ * @param desc - Structure containing the state of the DMA controller
+ * @param param - Initialization parameter for the DMA controller.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int no_os_dma_init(struct no_os_dma_desc **desc,
+		   struct no_os_dma_init_param *param)
+{
+	int ret;
+	uint32_t i, j;
+	void *mutex;
+
+	if (!param || !param->platform_ops)
+		return -EINVAL;
+
+	if (!param->platform_ops->dma_init)
+		return -ENOSYS;
+
+	no_os_mutex_init(&mutex);
+
+	no_os_mutex_lock(mutex);
+	ret = param->platform_ops->dma_init(desc, param);
+	if (ret)
+		goto unlock;
+
+	no_os_mutex_init(&(*desc)->mutex);
+
+	(*desc)->platform_ops = param->platform_ops;
+
+	for (i = 0; i < param->num_ch; i++) {
+		ret = no_os_list_init(&(*desc)->channels[i].sg_list, NO_OS_LIST_QUEUE, NULL);
+		if (ret)
+			goto list_err;
+
+		no_os_mutex_init(&(*desc)->channels[i].mutex);
+	}
+
+	(*desc)->ref++;
+	no_os_mutex_unlock(mutex);
+
+	return 0;
+
+list_err:
+	for (j = 0; j < i; j++)
+		no_os_list_remove((*desc)->channels[i].sg_list);
+
+	no_os_dma_remove(*desc);
+unlock:
+	no_os_mutex_unlock(mutex);
+
+	return ret;
+}
+
+/**
+ * @brief Remove resources allocated for the DMA controller.
+ * @param desc - Structure containing the state of the DMA controller
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int no_os_dma_remove(struct no_os_dma_desc *desc)
+{
+	uint32_t i;
+	int ret;
+
+	if (!desc)
+		return -EINVAL;
+
+	if (!desc->ref)
+		return 0;
+
+	for (i = 0; i < desc->num_ch; i++) {
+		ret = no_os_list_remove(desc->channels->sg_list);
+		if (ret)
+			return ret;
+
+		no_os_mutex_remove(desc->channels[i].mutex);
+	}
+
+	no_os_mutex_remove(desc->mutex);
+
+	ret = desc->platform_ops->dma_remove(desc);
+	if (ret)
+		return ret;
+
+	desc->ref--;
+
+	return 0;
+}
+
+/**
+ * @brief Get a free DMA channel.
+ * @param desc - Structure containing the state of the DMA controller.
+ * @param channel - Reference to the acquired channel.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int no_os_dma_acquire_channel(struct no_os_dma_desc *desc,
+			      struct no_os_dma_ch **channel)
+{
+	uint32_t ch_num;
+	int ret;
+
+	if (!desc || !desc->platform_ops)
+		return -EINVAL;
+
+	if (!desc->platform_ops->dma_acquire_ch)
+		return -ENOSYS;
+
+	no_os_mutex_lock(desc->mutex);
+
+	ret = desc->platform_ops->dma_acquire_ch(desc, &ch_num);
+	if (ret)
+		goto unlock;
+
+	*channel = &desc->channels[ch_num];
+
+unlock:
+	no_os_mutex_unlock(desc->mutex);
+
+	return ret;
+}
+
+/**
+ * @brief Free DMA channel.
+ * @param desc - Structure containing the state of the DMA controller.
+ * @param channel - Reference to the currently acquired channel.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int no_os_dma_release_channel(struct no_os_dma_desc *desc,
+			      struct no_os_dma_ch *channel)
+{
+	int ret;
+
+	if (!desc || !desc->platform_ops || !channel)
+		return -EINVAL;
+
+	if (!desc->platform_ops->dma_release_ch)
+		return -ENOSYS;
+
+	no_os_mutex_lock(channel->mutex);
+	ret = desc->platform_ops->dma_release_ch(desc, channel->id);
+	no_os_mutex_unlock(channel->mutex);
+
+	return ret;
+}
+
+/**
+ * @brief Acquire a channel and configure the list of transfers.
+ * @param desc - Structure containing the state of the DMA controller
+ * @param xfer - Array of DMA transfers.
+ * @param len - The number of transfers in the xfer list.
+ * @param ch - Previously acquired channel, for which the transfer will be configured.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int no_os_dma_config_xfer(struct no_os_dma_desc *desc,
+			  struct no_os_dma_xfer_desc *xfer,
+			  uint32_t len, struct no_os_dma_ch *ch)
+{
+	uint32_t i;
+	int ret;
+
+	struct no_os_callback_desc sg_callback = {
+		.callback = default_sg_callback,
+	};
+
+	if (!desc || !xfer || !len || !ch)
+		return -EINVAL;
+
+	no_os_mutex_lock(ch->mutex);
+
+	sg_callback.peripheral = xfer[0].periph;
+
+	switch (xfer[0].xfer_type) {
+	case MEM_TO_DEV:
+	case MEM_TO_MEM:
+		sg_callback.event = NO_OS_EVT_DMA_TX_COMPLETE;
+		break;
+	case DEV_TO_MEM:
+		sg_callback.event = NO_OS_EVT_DMA_RX_COMPLETE;
+		break;
+	default:
+		ret = -EINVAL;
+		goto unlock;
+	}
+
+	/*
+	 * Add the transfers to the channel's SG list. It's safe to do so, since
+	 * there are no ongoing transfers on this channel.
+	 */
+	for (i = 0; i < len; i++)
+		no_os_list_add_last(ch->sg_list, &xfer[i]);
+
+	if (desc->irq_ctrl) {
+		ch->irq_ctx.desc = desc;
+		ch->irq_ctx.channel = ch;
+		sg_callback.ctx = &ch->irq_ctx;
+		sg_callback.handle = (void *)ch->id;
+
+		ret = desc->platform_ops->dma_config_xfer(ch, xfer);
+		if (ret)
+			goto abort_xfer;
+
+		if (desc->sg_handler)
+			sg_callback.callback = desc->sg_handler;
+		else
+			sg_callback.callback = default_sg_callback;
+
+		ret = no_os_irq_register_callback(desc->irq_ctrl,
+						  ch->irq_num,
+						  &sg_callback);
+		if (ret)
+			goto abort_xfer;
+
+		no_os_irq_set_priority(desc->irq_ctrl, ch->irq_num, xfer[0].irq_priority);
+	}
+
+	return 0;
+
+abort_xfer:
+	no_os_dma_xfer_abort(desc, ch);
+unlock:
+	no_os_mutex_unlock(ch->mutex);
+
+	return ret;
+}
+
+/**
+ * @brief Lock a DMA channel, so it won't be acquired even if it's free.
+ * @param ch - Reference to the DMA channel
+ * @return 0 in case of success, -EINVAL if a NULL channel reference is provided.
+ */
+int no_os_dma_chan_lock(struct no_os_dma_ch *ch)
+{
+	no_os_mutex_lock(ch->mutex);
+
+	if (!ch)
+		return -EINVAL;
+
+	ch->sync_lock = true;
+
+	no_os_mutex_unlock(ch->mutex);
+
+	return 0;
+}
+
+/**
+ * @brief Unlock a DMA channel, marking it available for acquisition.
+ * @param ch - Reference to the DMA channel
+ * @return 0 in case of success, -EINVAL if a NULL channel reference is provided.
+ */
+int no_os_dma_chan_unlock(struct no_os_dma_ch *ch)
+{
+	no_os_mutex_lock(ch->mutex);
+
+	if (!ch)
+		return -EINVAL;
+
+	ch->sync_lock = false;
+
+	no_os_mutex_unlock(ch->mutex);
+
+	return 0;
+}
+
+/**
+ * @brief Acquire a channel and configure the list of transfers.
+ * @param desc - Structure containing the state of the DMA controller
+ * @param ch - Index for the channel we want to start the transfer on.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int no_os_dma_xfer_start(struct no_os_dma_desc *desc, struct no_os_dma_ch *ch)
+{
+	int ret;
+
+	if (!desc || !desc->platform_ops || !ch)
+		return -EINVAL;
+
+	if (!desc->platform_ops->dma_xfer_start)
+		return -ENOSYS;
+
+	no_os_mutex_lock(ch->mutex);
+
+	if (desc->irq_ctrl)
+		no_os_irq_enable(desc->irq_ctrl, ch->irq_num);
+
+	ret = desc->platform_ops->dma_xfer_start(desc, ch);
+
+	no_os_mutex_unlock(ch->mutex);
+
+	return ret;
+}
+
+/**
+ * @brief Abort the ongoing and any other future transfers scheduled for a channel.
+ * @param desc - Structure containing the state of the DMA controller
+ * @param ch - Index for the channel we want to abort the transfer.
+ * @return 0 in case of success, negative error code otherwise.
+ */
+int no_os_dma_xfer_abort(struct no_os_dma_desc *desc, struct no_os_dma_ch *ch)
+{
+	void *discard;
+	int ret;
+
+	if (!desc || !desc->platform_ops || !ch)
+		return -EINVAL;
+
+	if (!desc->platform_ops->dma_xfer_abort)
+		return -ENOSYS;
+
+	no_os_mutex_lock(ch->mutex);
+
+	if (desc->irq_ctrl)
+		no_os_irq_disable(desc->irq_ctrl, ch->irq_num);
+
+	do {
+		ret = no_os_list_get_first(ch->sg_list, &discard);
+	} while (!ret);
+
+	ret = desc->platform_ops->dma_xfer_abort(desc, ch);
+
+	no_os_mutex_unlock(ch->mutex);
+
+	return ret;
+}
+
+/**
+ * @brief Get the state of a DMA channel (free or not).
+ * @param desc - Reference to the DMA controller.
+ * @param ch - Reference to the DMA channel.
+ * @return true if a channel is free, false otherwise.
+ */
+bool no_os_dma_is_completed(struct no_os_dma_desc *desc,
+			    struct no_os_dma_ch *ch)
+{
+	(void)desc;
+
+	if (!ch)
+		return -EINVAL;
+
+	return ch->free;
+}
+
+/**
+ * @brief Whether or not there is a transfer in progress on a specific channel.
+ * @param desc - Reference to the DMA controller.
+ * @param ch - Reference to the DMA channel.
+ * @return true if a channel is free, false otherwise.
+ */
+bool no_os_dma_in_progress(struct no_os_dma_desc *desc, struct no_os_dma_ch *ch)
+{
+	int ret;
+
+	if (!desc || !desc->platform_ops || !ch)
+		return -EINVAL;
+
+	if (!desc->platform_ops->dma_ch_in_progress)
+		return -ENOSYS;
+
+	no_os_mutex_lock(ch->mutex);
+	ret = desc->platform_ops->dma_ch_in_progress(desc, ch);
+	no_os_mutex_unlock(ch->mutex);
+
+	return ret;
+}

include/no_os_dma.h
@@ -1,0 +1,269 @@
+/***************************************************************************//**
+ *   @file   no_os_dma.h
+ *   @brief  Platform independent function definitions and data types
+ * 	     for the DMA API.
+ *   @author Ciprian Regus (ciprian.regus@analog.com)
+********************************************************************************
+ * Copyright 2024(c) Analog Devices, Inc.
+ *
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *  - Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ *  - Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in
+ *    the documentation and/or other materials provided with the
+ *    distribution.
+ *  - Neither the name of Analog Devices, Inc. nor the names of its
+ *    contributors may be used to endorse or promote products derived
+ *    from this software without specific prior written permission.
+ *  - The use of this software may or may not infringe the patent rights
+ *    of one or more patent holders.  This license does not release you
+ *    from the requirement that you obtain separate licenses from these
+ *    patent holders to use this software.
+ *  - Use of the software either in source or binary form, must be run
+ *    on or directly connected to an Analog Devices Inc. component.
+ *
+ * THIS SOFTWARE IS PROVIDED BY ANALOG DEVICES "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, NON-INFRINGEMENT,
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL ANALOG DEVICES BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, INTELLECTUAL PROPERTY RIGHTS, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*******************************************************************************/
+#ifndef _NO_OS_DMA_H_
+#define _NO_OS_DMA_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+
+#include "no_os_list.h"
+#include "no_os_irq.h"
+#include "no_os_mutex.h"
+
+/**
+ * @enum no_os_dma_xfer_type
+ * @brief Supported transfer directions
+ */
+enum no_os_dma_xfer_type {
+	MEM_TO_MEM,
+	MEM_TO_DEV,
+	DEV_TO_MEM,
+};
+
+/**
+ * @struct no_os_dma_default_handler_data
+ * @brief IRQ parameter for the default inter transfer handler
+ */
+struct no_os_dma_default_handler_data {
+	struct no_os_dma_desc *desc;
+	struct no_os_dma_ch *channel;
+	void *extra;
+};
+
+struct no_os_dma_platform_ops;
+
+/**
+ * @struct no_os_dma_xfer_desc
+ * @brief It's used to setup a generic DMA transfer.
+ */
+struct no_os_dma_xfer_desc {
+	/** Source address for the data */
+	uint8_t *src;
+	/** Destination address for the data */
+	uint8_t *dst;
+	/** Transfer length in bytes */
+	uint32_t length;
+	/** Transfer direction */
+	enum no_os_dma_xfer_type xfer_type;
+
+	/** This function will be called once the transfer completes. */
+	void (*xfer_complete_cb)(struct no_os_dma_xfer_desc *,
+				 struct no_os_dma_xfer_desc *,
+				 void *);
+	/**
+	 * Parameter for the transfer complete callback. This data should
+	 * be valid at least until the callback is invoked.
+	 */
+	void *xfer_complete_ctx;
+	/** Transfer complete interrupt priority level */
+	uint32_t irq_priority;
+
+	/**
+	 * Peripheral which originated the transfer. Will be used to add
+	 * the callback to the proper event list.
+	 */
+	enum no_os_irq_peripheral periph;
+
+	/** User or platform defined data */
+	void *extra;
+};
+
+/**
+ * @struct no_os_dma_ch
+ * @brief Describes the state of a DMA channel.
+ */
+struct no_os_dma_ch {
+	/** Channel number */
+	uint32_t id;
+	/** Whether or not there is a transfer in progress on this channel */
+	bool free;
+	/** List of transfers for this channel */
+	struct no_os_list_desc *sg_list;
+	/** Channel specific interrupt line number */
+	uint32_t irq_num;
+	/** IRQ parameter for the default inter transfer handler */
+	struct no_os_dma_default_handler_data irq_ctx;
+	void *extra;
+
+	/** Used to synchronize channel specific operations */
+	void *mutex;
+
+	/**
+	 * Mark the channel as locked, in order to prevent it from being acquired
+	 * even if it's free. Used as a synchronization mechanism between channels.
+	 */
+	bool sync_lock;
+};
+
+/**
+ * @struct no_os_dma_desc
+ * @brief Describes the state of the DMA controller.
+ */
+struct no_os_dma_desc {
+	/** Unique id of a DMA controller */
+	uint32_t id;
+	/** The number of channels associated with the controller */
+	uint32_t num_ch;
+	/** References to channels associated with the controller */
+	struct no_os_dma_ch *channels;
+	/** Platform specific DMA functions */
+	struct no_os_dma_platform_ops *platform_ops;
+	/**
+	 * Interrupt controller configured by the platform specific DMA functions.
+	 * May be left uninitialized,Start resulting in the interrupt handler not being
+	 * called once a transfer completes. As such, the user (or platform code) has
+	 * to provide the cycling mechanism.
+	 */
+	struct no_os_irq_ctrl_desc *irq_ctrl;
+	/** Platform specific data */
+	void *extra;
+	/** Reference counter */
+	uint32_t ref;
+	/** Used to synchronize DMA controller specific operations */
+	void *mutex;
+
+	/**
+	 * Interrupt handler function called when a transfer is completed.
+	 * It's used to configure the DMA controller for the next transfer in
+	 * a channel's list.
+	 */
+	void (*sg_handler)(void *);
+};
+
+/**
+ * @struct no_os_dma_init_param
+ * @brief Initialization parameter for the DMA controller
+ */
+struct no_os_dma_init_param {
+	/** Unique id of a DMA controller */
+	uint32_t id;
+	/** The number of channels associated with the controller */
+	uint32_t num_ch;
+	/** Platform specific DMA functions */
+	struct no_os_dma_platform_ops *platform_ops;
+	/** Platform specific data */
+	void *extra;
+
+	/**
+	 * Optional custom scatter gather callback. May be set to NULL in
+	 * order to use the default one.
+	 */
+	void (*sg_handler)(void *);
+	/**
+	 * Optional custom parameter for the scatter gather callback. Only takes
+	 * effect if the handler is also provided.
+	 */
+	void *ctx;
+};
+
+/** Initialize a DMA controller. */
+int no_os_dma_init(struct no_os_dma_desc **,
+		   struct no_os_dma_init_param *);
+
+/** Free the resources allocated by the DMA layer. Disables the DMA controller. */
+int no_os_dma_remove(struct no_os_dma_desc *);
+
+/** Acquire a channel and add the transfers to it's SG list. */
+int no_os_dma_config_xfer(struct no_os_dma_desc *,
+			  struct no_os_dma_xfer_desc *,
+			  uint32_t,
+			  struct no_os_dma_ch *);
+
+/** Prevent a channel from being acquired, even if it's free. */
+int no_os_dma_chan_lock(struct no_os_dma_ch *);
+
+/** Allow a channel to be acquired, in case it's free. */
+int no_os_dma_chan_unlock(struct no_os_dma_ch *);
+
+/** Start the DMA transactions for a specific channel. */
+int no_os_dma_xfer_start(struct no_os_dma_desc *, struct no_os_dma_ch *);
+
+/**
+ * Stop the DMA transactions for a specific channel and remove the transfers
+ * from the SG list.
+ */
+int no_os_dma_xfer_abort(struct no_os_dma_desc *, struct no_os_dma_ch *);
+
+/**
+ * Check whether a transfer is complete. By default, it requires the interrupt
+ * handler to set the free flag for the channel.
+ */
+bool no_os_dma_is_completed(struct no_os_dma_desc *, struct no_os_dma_ch *);
+
+/** Whether or not a specific channel has an ongoing transfer. */
+bool no_os_dma_in_progress(struct no_os_dma_desc *, struct no_os_dma_ch *);
+
+/** Get a free DMA channel. */
+int no_os_dma_acquire_channel(struct no_os_dma_desc *, struct no_os_dma_ch **);
+
+/** Free a DMA channel. */
+int no_os_dma_release_channel(struct no_os_dma_desc *, struct no_os_dma_ch *);
+
+/**
+ * @struct no_os_irq_platform_ops
+ * @brief Structure holding IRQ function pointers that point to the platform
+ * specific function
+ */
+struct no_os_dma_platform_ops {
+	/** Initialize platform specific resources for the DMA controller. */
+	int (*dma_init)(struct no_os_dma_desc **, struct no_os_dma_init_param *);
+	/** Remove resources allocated for the DMA controller. */
+	int (*dma_remove)(struct no_os_dma_desc *desc);
+	/** Get a free channel and set it as busy. */
+	int (*dma_acquire_ch)(struct no_os_dma_desc *, uint32_t *);
+	int (*dma_release_ch)(struct no_os_dma_desc *, uint32_t);
+	/** Configure platform specific settings required for the current DMA transfer */
+	int (*dma_config_xfer)(struct no_os_dma_ch *,
+			       struct no_os_dma_xfer_desc *);
+	/** Signal the DMA controller to start the transfer. */
+	int (*dma_xfer_start)(struct no_os_dma_desc *, struct no_os_dma_ch *);
+	/** Signal the DMA controller to stop the transfer. */
+	int (*dma_xfer_abort)(struct no_os_dma_desc *, struct no_os_dma_ch *);
+	/** Whether or not a specific channel has an ongoing or future transfer configured */
+	bool (*dma_ch_is_completed)(struct no_os_dma_desc *, struct no_os_dma_ch *);
+	/**
+	 * Whether or not a specific channel has an ongoing transfer.
+	 * This returns true between transfers.
+	 */
+	bool (*dma_ch_in_progress)(struct no_os_dma_desc *, struct no_os_dma_ch *);
+};
+
+#endif

include/no_os_irq.h
@@ -75,6 +75,7 @@ enum no_os_irq_event {
 	NO_OS_EVT_TIM_PWM_PULSE_FINISHED,
 	NO_OS_EVT_DMA_RX_COMPLETE,
 	NO_OS_EVT_DMA_RX_HALF_COMPLETE,
+	NO_OS_EVT_DMA_TX_COMPLETE,
 };
 
 enum no_os_irq_trig_level {
@@ -91,7 +92,9 @@ enum no_os_irq_peripheral {
 	NO_OS_RTC_IRQ,
 	NO_OS_TIM_IRQ,
 	NO_OS_TDM_DMA_IRQ,
-	NO_OS_TIM_DMA_IRQ
+	NO_OS_TIM_DMA_IRQ,
+	NO_OS_SPI_DMA_IRQ,
+	NO_OS_DMA_IRQ,
 };
 
 /**

commit dfd978d8a9731395c91a2130e1f7e87d815d9697

drivers: dma: Fix DMA remove API
Fix to deallocate memory for list descriptors of all channels in remove API.

Fixes: bea3b2a68bbe ("Add a generic DMA API")

Signed-off-by: Naga Himanshu Indraganti <naga.indraganti@analog.com>

drivers/api/no_os_dma.c
@@ -152,7 +152,7 @@ int no_os_dma_remove(struct no_os_dma_desc *desc)
 		return 0;
 
 	for (i = 0; i < desc->num_ch; i++) {
-		ret = no_os_list_remove(desc->channels->sg_list);
+		ret = no_os_list_remove(desc->channels[i].sg_list);
 		if (ret)
 			return ret;
 